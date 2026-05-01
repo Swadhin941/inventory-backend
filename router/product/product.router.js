0b6ddb98a59f90892e4d1bbcdf6921a3220425f5
Swadhin Ghosh <swadhinghosh@Swadhins-MacBook-Air.local>
2026-05-01 21:50:13 +0600

    middlewared added on the brand routing

--- router/product/product.router.js
+++ router/product/product.router.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { addBrand, getAllBrand, updateBrand } = require("../../controller/product/product.controller");
 const { runValidation } = require("../../validators");
 const { productValidatorSchema } = require("../../validators/product.validator");
+const { verifyJWT } = require("../../middlewares/auth.middlewares");
 const productRouter = express.Router();
 
-productRouter.post("/add-brand",runValidation(productValidatorSchema.addBrandSchema), addBrand);
-productRouter.get("/get-all-brand", getAllBrand);
-productRouter.put("/update-brand", runValidation(productValidatorSchema.updateBrandSchema), updateBrand);
+productRouter.post("/add-brand",runValidation(productValidatorSchema.addBrandSchema),verifyJWT, addBrand);
+productRouter.get("/get-all-brand", verifyJWT, getAllBrand);
+productRouter.put("/update-brand", runValidation(productValidatorSchema.updateBrandSchema),verifyJWT, updateBrand);
 
 module.exports = productRouter;
